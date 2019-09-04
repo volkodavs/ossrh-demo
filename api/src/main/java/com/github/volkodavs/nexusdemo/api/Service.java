14b9f60b0ce0f69544a2350d6ee7e31033686f7f
Sergii Volkodav <volkodav_s@yahoo.com>
2019-09-04 21:34:27 +0100

    add deployment to nexus

--- api/src/main/java/com/github/volkodavs/nexusdemo/api/Service.java
+++ api/src/main/java/com/github/volkodavs/nexusdemo/api/Service.java
@@ -1,6 +1,10 @@
 package com.github.volkodavs.nexusdemo.api;
 
 public interface Service {
-
+    /**
+     *
+     * @param name user
+     * @return hello to the use
+     */
     String sayHello(String name);
 }
